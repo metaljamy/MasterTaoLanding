42a6718b5636ad94fc0dc9d9dd15fcb8b81e4d08
metaljamy <metaljamy@gmail.com>
2023-05-30 10:20:01 +0300

    Header +

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,7 +1,26 @@
 
-    $('.slider').slick();
-
-    Fancybox.bind("[data-fancybox]", {
-    // Your custom options
+$( document ).ready(function() {
+  $('.slider').slick({
+      dots: true,
+      speed: 1000,
+      appendArrows: ".container-arrows",
+      responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              arrows: false,
+              // fade: true,
+            }
+          },
+           
+      ]
   });
+
+  // $('.navbar-toggler').on('click', function(){
+  //   $('.navbar').toggleClass('margin-bottom')
+  // })
+
+
+});
+
 
